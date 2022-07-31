f611e0396c24e405b2c601972cd947cb8d179b79
Nate <nkasco@gmail.com>
2022-07-31 09:30:54 -0400

    General version currency, added CommunityToolkit.WinUI.UI.Controls to enable use with DataGrid control

--- Reporting.xaml.cs
+++ Reporting.xaml.cs
@@ -23,9 +23,40 @@ namespace ITATKWinUI
     /// </summary>
     public sealed partial class Reporting : Page
     {
+        System.Collections.ObjectModel.ObservableCollection<ReportingData> ReportData = new System.Collections.ObjectModel.ObservableCollection<ReportingData>();
+
         public Reporting()
         {
             this.InitializeComponent();
+
+            var records = MainWindow.GetReportingRecords();
+
+            foreach(ReportingRecord record in records)
+            {
+                ReportData.Add(new ReportingData(record.ScriptName, record.ScriptPath, record.HostExecuting, record.FileExtension, record.Target, record.UserExecuting, record.DateTime));
+            }
+        }
+    }
+
+    public class ReportingData
+    {
+        public string ScriptName { get; set; }
+        public string ScriptPath { get; set; }
+        public string HostExecuting { get; set; }
+        public string FileExtension { get; set; }
+        public string Target { get; set; }
+        public string UserExecuting { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public ReportingData(string ScriptName, string ScriptPath, string HostExecuting, string FileExtension, string Target, string UserExecuting, DateTime DateTime)
+        {
+            this.ScriptName = ScriptName;
+            this.ScriptPath = ScriptPath;
+            this.HostExecuting = HostExecuting;
+            this.FileExtension = FileExtension;
+            this.Target = Target;
+            this.UserExecuting = UserExecuting;
+            this.DateTime = DateTime;
         }
     }
 }
