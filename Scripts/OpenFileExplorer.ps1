d924356629ba3a533422fcd477dc5fdb6e03bf0d
Manjunath Beli <belibug@outlook.com>
2022-09-01 22:40:02 -0400

    Added script to make C$ function work. This works for both single device as well as multi device inputs.

---
+++ Scripts/OpenFileExplorer.ps1
@@ -0,0 +1,24 @@
+﻿function Invoke-FileShare {
+    param($device)
+    if (!(Test-Connection -TargetName $device -Count 2 -Quiet)) {
+        Write-Host "$device : not online"
+        return
+    }
+    else {
+        try { Invoke-Item "\\$device\c`$" -ErrorAction Stop }
+        catch { Write-Host "$device : Unable to launch c`$" -ForegroundColor Red }
+    }
+}
+
+$devInput = $args[0]
+
+if ($devInput.Contains(',')) {
+    ## Multi device input
+    $args -split ',' | ForEach-Object {
+        Invoke-FileShare -device $_
+    }
+    Read-Host 'Press enter to close'
+}
+else {
+    Invoke-FileShare -device $args[0]
+}
